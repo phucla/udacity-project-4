b3aca63e7365a2f4fe601f23e74d12d3071cadaf
PhucLa <laducphuc@gmail.com>
2023-05-25 11:09:02 +0700

    Add config

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+credential_path=~/password.txt
+dockerpath="phucla132/udacity"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat $credential_path | sudo docker login --username phucla132 --password-stdin
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
